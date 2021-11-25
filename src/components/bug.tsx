db4db2f3e5ae74c4e1d3711c5caea40dfc1398c2
Jose <santosjosel1995@gmail.com>
2021-11-24 22:40:28 -0500

    feat(): display for a bug

---
+++ src/components/bug.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+const Bug: React.FC<{ bug: any }> = ({ bug }) => {
+
+    const shade = classNames(
+        "list-group-item", "list-group-item-action", {
+        "list-group-item-success": bug.priority === "Low",
+        "list-group-item-warning": bug.priority === "Med",
+        "list-group-item-danger": bug.priority === "High",
+    })
+    const link = "/view/" + bug["_id"];
+
+    return (
+        <div>
+            <li className="d-flex flex-row position-relative">
+                <Link className={shade} to={link}>{bug.bugName}</Link>
+            </li>
+        </div>
+    )
+}
+
+export default Bug;
