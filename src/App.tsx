92e6deceafcd0d47c505f31bd144060dbfc60106
Jose <santosjosel1995@gmail.com>
2021-11-24 22:38:37 -0500

    update(): added other routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import NavBar from './components/navbar';
 import Home from './components/home';
+import CreateUser from './components/createUser';
+import BugList from './components/bugList';
+import ViewBug from './components/viewBug';
+import CreateBug from './components/createBug';
 
-const App: FunctionComponent<{}> = () => {
+const App: React.FC<{}> = () => {
     return (
         <div>
             <Router>
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/user" element={<CreateUser />} />
+                    <Route path="/bug" element={<BugList />} />
+                    <Route path="/view/:id" element={<ViewBug />} />
+                    <Route path="/create" element={<CreateBug />} />
                 </Routes>
             </Router>
         </div>
