1b4aa8a908a812853bf7c63b54d928e0a71f9df5
Jose <santosjosel1995@gmail.com>
2021-11-29 12:41:18 -0500

    chore(): type for passing useState

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 export interface User {
     id: string | number,
     username: string,
     email: string,
     password: string
+}
+export interface SetRefresh {
+    refresh: boolean,
+    setRefresh: React.Dispatch<React.SetStateAction<boolean>>
 }
