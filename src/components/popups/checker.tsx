4b5839d24f2c0d088c8a5b8fab9b4201246fc6d1
Jose <santosjosel1995@gmail.com>
2021-11-29 14:45:58 -0500

    feat(): Double check confirmation popup

---
+++ src/components/popups/checker.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Check } from "../../interfaces";
+
+const Checker: React.FC<Check> = ({ message, doThis }) => {
+    return (
+        <div>
+            <div className="modal" id="checkerBox" tabIndex={-1}>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Confirm Action</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>{message}</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={doThis}>Confirm</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Checker;
