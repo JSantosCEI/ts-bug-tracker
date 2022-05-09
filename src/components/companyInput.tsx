91a36b372ec69374d8ef9ed3f3e479c321602217
Jose <101306680+JSantosCEI@users.noreply.github.com>
2022-05-09 15:15:41 -0400

    feat(): text input for new company name

---
+++ src/components/companyInput.tsx
@@ -0,0 +1,19 @@
+import React from "react";  
+
+interface InputProps {
+    newCompany: string,
+    setNewCompany: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const CompanyInput: React.FC<InputProps> = ({ newCompany, setNewCompany}) => {
+    return (
+        <div>
+            <input type="email" value={newCompany} name="company"
+                className="form-control" placeholder="Enter New Company Name"
+                onChange={e => setNewCompany(e.target.value)} 
+            />
+        </div>
+    )
+}
+
+export default CompanyInput;
