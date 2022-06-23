1e5757e572c1525443ac56703f974f1851b312b4
Jose <101306680+JSantosCEI@users.noreply.github.com>
2022-06-23 12:44:41 -0500

    Update(): Added React Query for API calls

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,18 @@ import ReactDOM from "react-dom";
 import App from './App';
 import './App.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import React from "react";
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-    <App />,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    </React.StrictMode>,
     document.getElementById('root')
 );
