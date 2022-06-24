e607c7b0fcb506f259bb912f0f1c0bd4b66b0a9a
Jose <101306680+JSantosCEI@users.noreply.github.com>
2022-06-24 15:41:41 -0500

    refactor(): API calls are handled by React Query

--- src/pages/bugs/bugList.tsx
+++ src/pages/bugs/bugList.tsx
@@ -1,123 +1,114 @@
-import axios from "axios";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Navigate } from "react-router";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 import Bug from "../../components/bug";
 import AddBug from "../../components/popups/addBug";
 import { UserContext } from "../../components/userContext";
-import { authUser } from "../../api/userApi";
-import { getUserBugs } from "../../api/bugApi";
+import { addBug, getAllBugsByToken, getUserByToken } from "../../api/bugTrackerApi";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Spinner  from "../../components/utilities/spinner";
+import { Bug as BugSchema } from "../../interfaces";
 
 library.add(faPlus);
 
 const BugList: React.FC = () => {
-    const [username, setUsername] = useState<string>('');
-    const [bugs, setBugs] = useState<Array<any>>([]);
     const [refresh, setRefresh] = useState<boolean>(false);
-    const [expired, setExpired] = useState<boolean>(false);
-    const { user, setUser } = useContext(UserContext);
+    const { user } = useContext(UserContext);
+    const queryClient = useQueryClient();
+    
+    const {data: userData, isLoading: userLoading} = useQuery('userInfo', () => getUserByToken(user));
+    const { isLoading, isError, data } = useQuery('bugs', () => getAllBugsByToken(user));
 
-    useEffect(() => {
-        axios.post(authUser + user, {"token": user}, { headers: { 'Authorization': `Bearer ${user}` } })
-            .then((res) => {
-                //console.log(res.data);
-                setUsername(res.data.username);
-                axios.get(getUserBugs + res.data.userId, { headers: { Authorization: `Bearer ${user}` } })
-                    .then(res => {
-                        //console.log(res.data);
-                        setBugs([...res.data]);
-                    })
-                    .catch((err) => console.error(err));
-            })
-            .catch((err) => {
-                setUser('');
-                let message: string = err.message;
-                let code: number = parseInt(message.slice(-3));
-                code === 400 ? setExpired(true) : console.error(err);
-            });
-    }, [refresh, user, setUser])
+    const addBugMutation = useMutation((bug: BugSchema) => addBug(bug, user), {
+        onSuccess: () => {
+            queryClient.invalidateQueries("bugs");
+        }
+    })
 
-    const unassignedList = bugs
-        .filter((bug) => bug.status === "Unassigned")
-        .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
+    if(isLoading || userLoading){
+        return <Spinner />;
+    }
 
-    const toDoList = bugs
+    if(isError || data === undefined || userData === undefined){
+        return <Navigate to="/user" state={{ newUser: false, expired: true }} />
+    }
+    
+    const unassignedList = data
+    .filter((bug) => bug.status === "Unassigned")
+    .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
+
+    const toDoList = data
         .filter((bug) => bug.status === "To Do")
         .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
 
-    const inProgressList = bugs
+    const inProgressList = data
         .filter((bug) => bug.status === "In Progress")
         .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
 
-    const qaList = bugs
+    const qaList = data
         .filter((bug) => bug.status === "QA")
         .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
 
-    const completeList = bugs
+    const completeList = data
         .filter((bug) => bug.status === "Complete")
         .map((bug) => <Bug key={bug["bugId"]} bug={bug} />)
-
+    
     return (
         <div>
-            {
-                !user || expired ?
-                    <Navigate to="/user" state={{ newUser: false, expired: true }} />
-                    :
-                    <div className="container my-4 bugList">
-                        <div className="d-flex justify-content-between align-items-baseline">
-                            <h1 className="text-capitalize">{username} Bug List</h1>
-                            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"><FontAwesomeIcon icon={faPlus} size="2x" /></button>
-                        </div>
-                        <AddBug refresh={refresh} setRefresh={setRefresh} />
-                        <br />
-                        <div className="d-grid gap-5">
-                            <div className="row row-col-auto pt-1 justify-content-center" style={{ minHeight: "20vh" }}>
-                                {(toDoList.length > 0) &&
-                                    <ul className="list-group col-sm-4">
-                                        <h4 className="align-self-center mt-2 mb-3">To Do</h4>
-                                        {toDoList}
-                                    </ul>
-                                }
-                                {(inProgressList.length > 0) &&
-                                    <ul className="list-group col-sm-4">
-                                        <h4 className="align-self-center mt-2 mb-3">In Progress</h4>
-                                        {inProgressList}
-                                    </ul>
-                                }
-                                {(qaList.length > 0) &&
-                                    <ul className="list-group col-sm-4">
-                                        <h4 className="align-self-center mt-2 mb-3">Waiting On QA</h4>
-                                        {qaList}
-                                    </ul>
-                                }
-                            </div>
-                            {(unassignedList.length > 0) &&
-                                <div className="row row-cols border-top pt-1">
-                                    <ul className="list-group col">
-                                        <h4 className="align-self-center mt-2 mb-3">Unassgined</h4>
-                                        <div className="row row-cols-1 row-cols-sm-3">
-                                            {unassignedList}
-                                        </div>
-                                    </ul>
-                                </div>
-                            }
-                            {(completeList.length > 0) &&
-                                <div className="row row-cols border-top pt-1">
-                                    <ul className="list-group col">
-                                        <h4 className="align-self-center mt-2 mb-3">Completed</h4>
-                                        <div className="row row-cols-1 row-cols-sm-3">
-                                            {completeList}
-                                        </div>
-                                    </ul>
-                                </div>
-                            }
-                        </div>
+            <div className="container my-4 bugList">
+                <div className="d-flex justify-content-between align-items-baseline">
+                    <h1 className="text-capitalize">{userData.username} Bug List</h1>
+                    <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"><FontAwesomeIcon icon={faPlus} size="2x" /></button>
+                </div>
+                <AddBug refresh={refresh} setRefresh={setRefresh} />
+                <br />
+                <div className="d-grid gap-5">
+                    <div className="row row-col-auto pt-1 justify-content-center" style={{ minHeight: "20vh" }}>
+                        {(toDoList.length > 0) &&
+                            <ul className="list-group col-sm-4">
+                                <h4 className="align-self-center mt-2 mb-3">To Do</h4>
+                                {toDoList}
+                            </ul>
+                        }
+                        {(inProgressList.length > 0) &&
+                            <ul className="list-group col-sm-4">
+                                <h4 className="align-self-center mt-2 mb-3">In Progress</h4>
+                                {inProgressList}
+                            </ul>
+                        }
+                        {(qaList.length > 0) &&
+                            <ul className="list-group col-sm-4">
+                                <h4 className="align-self-center mt-2 mb-3">Waiting On QA</h4>
+                                {qaList}
+                            </ul>
+                        }
                     </div>
-            }
+                    {(unassignedList.length > 0) &&
+                        <div className="row row-cols border-top pt-1">
+                            <ul className="list-group col">
+                                <h4 className="align-self-center mt-2 mb-3">Unassgined</h4>
+                                <div className="row row-cols-1 row-cols-sm-3">
+                                    {unassignedList}
+                                </div>
+                            </ul>
+                        </div>
+                    }
+                    {(completeList.length > 0) &&
+                        <div className="row row-cols border-top pt-1">
+                            <ul className="list-group col">
+                                <h4 className="align-self-center mt-2 mb-3">Completed</h4>
+                                <div className="row row-cols-1 row-cols-sm-3">
+                                    {completeList}
+                                </div>
+                            </ul>
+                        </div>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
