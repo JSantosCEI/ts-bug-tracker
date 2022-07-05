f1e1437951a30b807afd75223e3135153fb913ff
Jose <101306680+JSantosCEI@users.noreply.github.com>
2022-07-05 13:12:02 -0400

    update(): needed some margin

--- src/components/utilities/spinner.tsx
+++ src/components/utilities/spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner: React.FC = () => {
     return (
-        <div className="text-center">
+        <div className="text-center mt-1">
             <div className="spinner-border text-primary" style={{ "width": "3rem", "height": "3rem" }} role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
